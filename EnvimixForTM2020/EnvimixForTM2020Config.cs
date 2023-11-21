4340d95d8614f003340345b17de7186baa8aa41b
Petr Pivoňka <petrpiv1@gmail.com>
2023-11-21 23:22:30 +0100

    Add dash and validation mode to real

--- EnvimixForTM2020/EnvimixForTM2020Config.cs
+++ EnvimixForTM2020/EnvimixForTM2020Config.cs
@@ -4,10 +4,10 @@ namespace EnvimixForTM2020;
 
 public class EnvimixForTM2020Config : Config
 {
-    public string MapNameFormat { get; set; } = "$<{0}$>.{1}";
+    public string MapNameFormat { get; set; } = "$<{0}$> - {1}";
     public bool IncludeCarSport { get; set; } = true;
     public bool IncludeCarSnow { get; set; } = true;
     public bool IncludeCharacterPilot { get; set; } = false;
-    public ValidationMode ValidationMode { get; set; }
+    public ValidationMode ValidationMode { get; set; } = ValidationMode.Real;
     public bool GenerateDefaultCarVariant { get; set; }
 }
