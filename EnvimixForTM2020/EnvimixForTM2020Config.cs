b106ead99a0fb453cac71eb237499572835e3e76
Petr Pivoňka <petrpiv1@gmail.com>
2024-02-26 19:24:45 +0100

    Add DesertCar and gate replacement

--- EnvimixForTM2020/EnvimixForTM2020Config.cs
+++ EnvimixForTM2020/EnvimixForTM2020Config.cs
@@ -7,7 +7,8 @@ public class EnvimixForTM2020Config : Config
     public string MapNameFormat { get; set; } = "$<{0}$> - {1}";
     public bool IncludeCarSport { get; set; } = true;
     public bool IncludeCarSnow { get; set; } = true;
-    public bool IncludeCarRally { get; set; } = false;
+    public bool IncludeCarRally { get; set; } = true;
+    public bool IncludeCarDesert { get; set; } = false;
     public bool IncludeCharacterPilot { get; set; } = false;
     public ValidationMode ValidationMode { get; set; } = ValidationMode.Real;
     public bool GenerateDefaultCarVariant { get; set; }
